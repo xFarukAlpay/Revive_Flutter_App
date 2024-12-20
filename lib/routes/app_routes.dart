34004c5ee06cca136496cbef49fd9148305153a3
DevFaruk <faruk16alpay@gmail.com>
2024-12-20 17:01:11 +0300

    ilan shared added paged finished

--- lib/routes/app_routes.dart
+++ lib/routes/app_routes.dart
@@ -6,6 +6,8 @@ import '../presentation/login_ekran_screen/login_ekrann_screen.dart';
 import '../presentation/profil_screen/profil_screen.dart';
 import '../presentation/signup_ekran_screen/signup_ekran_screen.dart';
 import '../presentation/ilan_ver/ilan_ver_screen.dart';
+import '../presentation/ilan_ver_detail/ilan_ver_detail_screen.dart';
+import '../presentation/ilan_ver_shared/ilan_ver_shared_screen.dart';
 
 class AppRoutes{
   static const String giriEkranScreen = 'giri_ekran_screen';
@@ -21,6 +23,8 @@ class AppRoutes{
   static const String ilanScreen = '/ilan_screen';
 
   static const String ilanVer = '/ilan_ver';
+  static const String ilanVerDetail = '/ilan_ver_detail';
+  static const String ilanVerShared = '/ilan_ver_shared';
 
 
 
@@ -35,7 +39,8 @@ class AppRoutes{
     profilScreen: ProfilScreen.builder,
     ilanScreen: IlanScreen.builder,
     ilanVer: IlanVerScreen.builder,
+    ilanVerDetail: IlanVerDetailScreen.builder, // Hata burada düzeltildi
+    ilanVerShared: IlanVerSharedScreen.builder, // Hata burada düzeltildi
     appNavigationScreen: AppNavigationScreen.builder,
-    initialRoute: GiriEkranScreen.builder
-  };
-}
+    initialRoute: GiriEkranScreen.builder,
+  };}
